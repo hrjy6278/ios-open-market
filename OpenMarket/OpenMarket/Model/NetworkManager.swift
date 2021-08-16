39a017b7fb4f642a2e51b5e9f646503614f5c70e
Ldoy <leedoy7989@gmail.com>
2021-08-16 13:05:36 +0900

    확인중

--- OpenMarket/OpenMarket/Model/NetworkManager.swift
+++ OpenMarket/OpenMarket/Model/NetworkManager.swift
@@ -81,3 +81,54 @@ struct NetworkManager {
         }.resume()
     }
 }
+
+//
+//func createHTTPBody(withParameters para: [String: Any]?, image: [Media]?, boundary: String) -> Data {
+//
+//    let lineBreak = "\r\n" // 개행
+//    var body = Data() // 데이터 담을 곳
+//
+//    // key-value의 사전형태로 post할 정보가 파라미터로 들어감
+//    if let parameters = para {
+//        for (key, value) in parameters {
+//            body.append("--\(boundary + lineBreak)")
+//            body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
+//            body.append("\(value)\(lineBreak)")
+//        }
+//    }
+//    // 이미지의 경우 따로 처리해준다.
+//    if let image = image {
+//    for photo in image {
+//        body.append("--\(boundary + lineBreak)")
+//        body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.filename)\"\(lineBreak)")
+//        body.append("Content-Type: \(photo.mimeType +  lineBreak + lineBreak)")
+//        body.append(photo.data)
+//        body.append(lineBreak)
+//    }
+//    }
+//    //body끝나는 부분은 -- 를 말미에 추가해준다.
+//    body.append("--\(boundary)--\(lineBreak)")
+////        print(body)
+//    return body
+//}
+//
+//@IBAction func sendPost(_ sender: Any) {
+//    //MARK:-Step4
+////        guard let testImages = UIImage(named: "2") else { return }
+////        let imagesArray = [Media(withImage: testImages, forkey: "testImage", mimeType: "image/jpeg", filename: "phototestImage.jpeg")]
+//
+//    let img = #imageLiteral(resourceName: "cat")
+//    guard let testImage = Media(withImage: img, forkey: "images[]", mimeType: "image/jpg", filename: "asdf.jpg") else { return }
+//    let parameters: [String: Any] = ["title": "테스트", "descriptions": "테스트제발성공", "price" : 100, "stock": 100, "discounted_price": 10, "currency": "Euro", "password": "12345"]
+//
+//    //메소드 해당 부분
+//    guard let url = URL(string: "https://camp-open-market-2.herokuapp.com/item") else { return }
+//    var request = URLRequest(url: url)
+//    request.httpMethod = "POST"
+//
+//    let boundary = generateBoundary()
+//
+//    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+////        request.addValue("", forHTTPHeaderField: "Content-Encoding")
+//
+//
--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -13,13 +13,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func post(_ sender: Any) {
-        let session = URLSession()
+    @IBAction func postButton(_ sender: Any) {
+        let session = URLSession.shared
         let test = NetworkManager(session: session)
         let url = URL(string: "https://camp-open-market-2.herokuapp.com/item")!
-        let par = []
-        test.createHTTPBody(with: , media: <#T##[Media]?#>)
-        test.request(httpMethod: HTTPMethod.post, url: url, body: <#T##Data?#>, completionHandler: <#T##(Result<Data, NetworkError>) -> ()#>)
+        let img = UIImage(named: "cat")
+        let imgMedia = Media(image: img!, key: .images, mimeType: .jpeg, fileName: "cat")
+        
+//                guard let testImages = UIImage(named: "2") else { return }
+//        //        let imagesArray = [Media(withImage: testImages, forkey: "testImage", mimeType: "image/jpeg", filename: "phototestImage.jpeg")]
+//
+//            guard let testImage = Media(withImage: img, forkey: "images[]", mimeType: "image/jpg", filename: "asdf.jpg") else { return }
+        
+            let parameters: [String: Any] = ["title": "테스트", "descriptions": "테스트제발성공", "price" : 100, "stock": 100, "discounted_price": 10, "currency": "Euro", "password": "12345"]
+        
+        
+        let body = test.createHTTPBody(with: parameters, media: [])
+        test.request(httpMethod: HTTPMethod.post, url: url, body: body) { result in
+            print(result)
+        }
     }
     
 }
