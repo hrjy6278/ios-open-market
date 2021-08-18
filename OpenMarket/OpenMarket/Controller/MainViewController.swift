48f1f0179171a6708c7a248718ee97c3e1c4f6e9
Ldoy <leedoy7989@gmail.com>
2021-08-18 22:39:24 +0900

    페이지1 로드 성공

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -10,51 +10,59 @@ class MainViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     var openMarketItemList: OpenMarketItems?
-//    var openMarketItem: OpenMarketItems.Item?
     
+    func test() {
+        let urlSession = URLSession.shared
+        let networkManager = NetworkManager(session: urlSession)
+        let url = URL(string: "https://camp-open-market-2.herokuapp.com/items/1")!
+        
+        networkManager.request(httpMethod: .get, url: url, body: nil, .json) { data in
+            switch data {
+            case .failure(let error):
+                print(error)
+            case .success(let result):
+                let jsonData = try? ParsingManager.jsonDecode(data: result, type: OpenMarketItems.self)
+                DispatchQueue.main.async {
+                    self.openMarketItemList = jsonData
+                    
+                    self.collectionView.reloadData()
+                    // discard the dataSource and delegate data and requery as necessary
+                }
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
-        
-        let networkManager = NetworkManager(session: URLSession.shared)
-        let url = URL(string: "https://camp-open-market-2.herokuapp.com/items/1")!
-        
-        networkManager.request(httpMethod: .get, url: url, body: nil, nil) { result in
-            
-            switch result {
-            case .failure(let error):
-                print(error)
-            case .success(let success):
-                self.openMarketItemList = try ParsingManager.jsonDecode(data: success, type: OpenMarketItems.self)
-            }
-            
-        }
+        test()
+        setupCollectionViewLayOut()
         
     }
 }
 
 extension MainViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
-    }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        openMarketItemList?.items.count ?? 0
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as? ItemCell else { return UICollectionViewCell()}
 
-        let networkManager = NetworkManager.init(session: URLSession.shared)
-
+        let openMarketItemArray = openMarketItemList?.items
+        let networkManager = NetworkManager(session: URLSession.shared)
         
-        let openMarketItems = openMarketItemList?.items.flatMap { $0 }
-        let thumbnailUrl = openMarketItems?[indexPath.item].thumbnails.first
+        let thumbnailUrl = openMarketItemArray?[indexPath.item].thumbnails.first
+        
         guard let url = URL(string: thumbnailUrl!) else { return UICollectionViewCell() }
-       
+        
         networkManager.request(httpMethod: .get, url: url, body: nil, nil) { result in
             switch result {
             case .success(let data):
                 guard let image = UIImage(data: data) else { return }
                 DispatchQueue.main.async {
-                    cell.update(openMarketItems?[indexPath.row])
-
+                    cell.update(openMarketItemArray?[indexPath.item])
                     cell.updateImage(image)
                 }
             case .failure(let error):
@@ -62,9 +70,19 @@ extension MainViewController: UICollectionViewDataSource {
             }
         }
         return cell
-    
     }
-
-
+    
+    
+    func setupCollectionViewLayOut() {
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        flowLayout.minimumLineSpacing = 8
+        flowLayout.minimumInteritemSpacing = 8
+        flowLayout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        
+        let halfWidth = view.bounds.width / 2.2
+        let halfHeight = view.bounds.height / 2
+        flowLayout.itemSize = CGSize(width: halfWidth , height: halfHeight - 50)
+        collectionView.collectionViewLayout = flowLayout
+    }
 }
-
--- OpenMarket/OpenMarket/View/ItemCell.swift
+++ OpenMarket/OpenMarket/View/ItemCell.swift
@@ -13,21 +13,48 @@ class ItemCell: UICollectionViewCell {
     @IBOutlet weak var titleText: UILabel!
     @IBOutlet weak var priceText: UILabel!
     @IBOutlet weak var statusText: UILabel!
+    @IBOutlet weak var discountedPrice: UILabel!
+    
+    func strockLabel(_ item: OpenMarketItems.Item) {
+        
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "\(item.price)")
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+            priceText?.attributedText = attributeString
+            priceText?.textColor = .red
+        }
+    
     
     func update(_ item: OpenMarketItems.Item?) {
-        priceText?.text = item?.currency ?? "Won" + "\(item?.price)"
-        titleText.text = item?.title
-        if item?.stock == 0 {
-            statusText.text = "품절"
-        } else {
-            statusText.text = "\(item?.stock)"
+        guard let item = item else { return }
+        
+        priceText?.text = "\(item.currency)\(item.price)"
+        titleText?.text = item.title
+        statusText?.text = "잔여수량: \(item.stock)"
+        discountedPrice?.isHidden = true
+        
+        if item.stock == 0 {
+            statusText?.text = "품절"
+            statusText?.textColor = .systemYellow
+            discountedPrice?.isHidden = true
         }
+        
+        if let discounted = item.discountedPrice {
+            strockLabel(item)
+            discountedPrice?.text = "\(item.currency)\(discounted)"
+            discountedPrice?.isHidden = false
 
+        }
     }
     
     func updateImage(_ image: UIImage) {
         listImage.image = image
     }
-
-
+    
+    override func prepareForReuse() {
+//        self.discountedPrice = nil
+//        self.priceText = nil
+//        self.statusText = nil
+//        self.titleText = nil
+    }
+    
 }
