82cd2f51b6d7a656a8115d200abd8be24c268042
Ldoy <leedoy7989@gmail.com>
2021-08-24 19:05:11 +0900

    무한로딩

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,13 +8,56 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+    
     let test1 = OpenMarketDataSource.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = test1
         let layout = Layout.generate(self.view)
         collectionView.collectionViewLayout = layout
         collectionView.reloadData()
+        collectionView.delegate = self
     }
+    var isLoading = false
+    var page = 2
+    
 }
 
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.item == (test1.openMarketItemList.count - 1) {
+            
+            OpenMarketLoadData().requestOpenMarketMainPageData(page: "2") { items in
+                self.test1.openMarketItemList.append(items)
+            }
+            //            collectionView.reloadData()
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scroll = scrollView
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        
+        if (offsetY > contentHeight - scrollView.frame.height * 4) && !isLoading {
+            loadMoreData("\(page)")
+            page += 1
+        }
+        
+    }
+    
+    func loadMoreData(_ page: String) {
+        if !self.isLoading {
+            self.isLoading = true
+            OpenMarketLoadData().requestOpenMarketMainPageData(page: page) { items in
+                self.test1.openMarketItemList.append(items)
+            }
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+                self.isLoading = false
+            }
+        }
+    }
+    
+}
